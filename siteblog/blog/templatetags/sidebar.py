d63e1f93216478f182524d0e5f004534c6f90bcb
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-02-08 11:59:06 +0300

    Реализовал вывод облака тегов и популярных постов

--- siteblog/blog/templatetags/sidebar.py
+++ siteblog/blog/templatetags/sidebar.py
@@ -4,6 +4,11 @@ from blog.models import Post, Tag
 register = template.Library()
 
 @register.inclusion_tag('blog/popular_posts_tpl.html')
-def get_popular(count=3)
+def get_popular(count=3):
     posts = Post.objects.order_by('-views')[:count]
     return {'posts': posts}
+
+@register.inclusion_tag('blog/tags_tpl.html')
+def get_tags():
+    tags = Tag.objects.all()
+    return {"tags": tags}
